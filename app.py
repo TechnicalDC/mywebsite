d823c885c28843c7f74966e72ef76d0a7c3878d9
Dilip Chauhan <chauhandilip305@gmail.com>
2022-08-02 22:10:35 +0530

    Updated on 02 August 2022

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    data = {
+        "title": "technicaldc",
+        "name" : "Welcome"
+    }
+    return render_template("index.html", data = data)
 
 if __name__ == '__main__':
     app.run(debug = True)
