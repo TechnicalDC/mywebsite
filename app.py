81455a1162ceb622d80cd757f27307d971d91034
Dilip Chauhan <chauhandilip305@gmail.com>
2022-08-03 22:40:53 +0530

    Updated on 03 August 2022

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///app.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Sections(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String, nullable = False)
 
 @app.route('/')
-def hello_world():
+def homepage():
     data = {
         "title": "technicaldc",
         "name" : "Welcome"
