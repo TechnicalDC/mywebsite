e76cc7875bdf6553af9e8e8f26a1fc8fa47e8478
Dilip Chauhan <chauhandilip305@gmail.com>
2022-08-15 14:41:50 +0530

    Updated on 15 August 2022

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, request
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, ValidationError
+from wtforms import StringField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///app.db"
@@ -13,11 +15,16 @@ app.config['SECRET_KEY'] = "HelloWorld"
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-class SectionForm(FlaskForm):
-    name = StringField('Section name', validators=[DataRequired()])
-    url = StringField('Url', validators=[DataRequired()])
-    icon = StringField('Icon', validators=[DataRequired()])
+class ContactForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired()])
+    message = TextAreaField('Message', validators=[DataRequired()])
     submit = SubmitField("Submit")
+
+class Configuation(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    conf_name = db.Column(db.String, unique = True)
+    conf_value = db.Column(db.String, nullable = False)
 
 class Sections(db.Model):
     id = db.Column(db.Integer, primary_key = True)
@@ -48,6 +55,14 @@ class Projects(db.Model):
     desc = db.Column(db.Text, nullable = False)
     url = db.Column(db.Text, nullable = False)
 
+admin_area = Admin(app)
+admin_area.add_view(ModelView(Configuation, db.session))
+admin_area.add_view(ModelView(Sections, db.session))
+admin_area.add_view(ModelView(About, db.session))
+admin_area.add_view(ModelView(Skills, db.session))
+admin_area.add_view(ModelView(Projects, db.session))
+admin_area.add_view(ModelView(Contact, db.session))
+
 @app.route('/')
 def homepage():
     sections = {}
@@ -56,57 +71,57 @@ def homepage():
     }
     return render_template("index.html", data = data)
 
-@app.route('/admin',methods=['POST', 'GET'])
-def admin():
-    form = SectionForm()
-    sections = Sections()
-    name = None
-    url = None
-    icon = None
-    if request.method == 'POST' and form.validate_on_submit():
-        name = form.name.data
-        if Sections.query.filter_by(name = name).first():
-            raise ValidationError('Name must be less than 50 characters')
-        url = form.url.data
-        icon = form.icon.data
-        sections = Sections(name = name, url = url, icon = icon)
-        db.session.add(sections)
-        db.session.commit()
-    data = Sections.query.all ()
-    return render_template("admin.html", data = data, form = form)
+# @app.route('/admin',methods=['POST', 'GET'])
+# def admin():
+#     form = SectionForm()
+#     sections = Sections()
+#     name = None
+#     url = None
+#     icon = None
+#     if request.method == 'POST' and form.validate_on_submit():
+#         name = form.name.data
+#         if Sections.query.filter_by(name = name).first():
+#             raise ValidationError('Name must be less than 50 characters')
+#         url = form.url.data
+#         icon = form.icon.data
+#         sections = Sections(name = name, url = url, icon = icon)
+#         db.session.add(sections)
+#         db.session.commit()
+#     data = Sections.query.all()
+#     return render_template("admin.html", data = data, form = form)
 
 @app.route('/home')
 def home():
-    sections = {}
-    data = {
-        "sections": sections,
-    }
+    data = Sections.query.all()
     return render_template("home.html", data = data)
 
 @app.route('/about')
 def about():
-    sections = {}
-    data = {
-        "sections": sections,
-        "details" : "Hey, I'm Dilip Chauhan, a programmer"
-    }
+    data = About.query.filter_by(default = True).first()
     return render_template("about.html", data = data)
 
 @app.route('/skills')
 def skills():
-    sections = {}
-    data = {
-        "sections": sections,
-    }
-    return render_template("skills.html", data = data)
+    skills = Skills.query.all()
+    return render_template("skills.html", data = skills)
 
-@app.route('/contact')
+@app.route('/contact', methods=['POST', 'GET'])
 def contact():
-    sections = {}
-    data = {
-        "sections": sections,
-    }
-    return render_template("contact.html", data = data)
+    form = ContactForm()
+    name = None
+    email = None
+    message = None
+    if request.method == 'POST' and form.validate_on_submit():
+        name = form.name.data
+        email = form.email.data
+        message = form.message.data
+        contact = Contact(name = name, email = email, message = message)
+        db.session.add(contact)
+        db.session.commit()
+        form.name.data = None
+        form.email.data = None
+        form.message.data = None
+    return render_template("contact.html", form = form)
 
 
 if __name__ == '__main__':
