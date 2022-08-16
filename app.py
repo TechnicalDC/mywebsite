be8683ed651d6db4df5e3361a3a5eecd89ae43b3
Dilip Chauhan <chauhandilip305@gmail.com>
2022-08-16 23:32:47 +0530

    Updated on 16 August 2022

--- app.py
+++ app.py
@@ -31,10 +31,10 @@ class Sections(db.Model):
     name = db.Column(db.String, unique = True, nullable = False)
     url = db.Column(db.String, nullable = False)
     icon = db.Column(db.String, nullable = False)
+    is_active = db.Column(db.Boolean, default = True)
 
 class About(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    section = db.Column(db.String, nullable = False)
     text = db.Column(db.Text, nullable = False)
     default = db.Column(db.Boolean, nullable = False)
 
@@ -64,63 +64,45 @@ admin_area.add_view(ModelView(Projects, db.session))
 admin_area.add_view(ModelView(Contact, db.session))
 
 @app.route('/')
-def homepage():
-    sections = {}
-    data = {
-        "sections": sections,
-    }
-    return render_template("index.html", data = data)
-
-# @app.route('/admin',methods=['POST', 'GET'])
-# def admin():
-#     form = SectionForm()
-#     sections = Sections()
-#     name = None
-#     url = None
-#     icon = None
-#     if request.method == 'POST' and form.validate_on_submit():
-#         name = form.name.data
-#         if Sections.query.filter_by(name = name).first():
-#             raise ValidationError('Name must be less than 50 characters')
-#         url = form.url.data
-#         icon = form.icon.data
-#         sections = Sections(name = name, url = url, icon = icon)
-#         db.session.add(sections)
-#         db.session.commit()
-#     data = Sections.query.all()
-#     return render_template("admin.html", data = data, form = form)
-
-@app.route('/home')
 def home():
-    data = Sections.query.all()
-    return render_template("home.html", data = data)
+    sections = Sections.query.filter_by(is_active = True).all()
+    return render_template("home.html", sections = sections)
 
 @app.route('/about')
 def about():
-    data = About.query.filter_by(default = True).first()
+    data = None
+    section = Sections.query.filter_by(name = "about", is_active = True).first()
+    if section is not None: 
+        data = About.query.filter_by(default = True).first()
     return render_template("about.html", data = data)
 
 @app.route('/skills')
 def skills():
-    skills = Skills.query.all()
+    skills = None
+    section = Sections.query.filter_by(name = "skills", is_active = True).first()
+    if section is not None: 
+        skills = Skills.query.all()
     return render_template("skills.html", data = skills)
 
 @app.route('/contact', methods=['POST', 'GET'])
 def contact():
-    form = ContactForm()
-    name = None
-    email = None
-    message = None
-    if request.method == 'POST' and form.validate_on_submit():
-        name = form.name.data
-        email = form.email.data
-        message = form.message.data
-        contact = Contact(name = name, email = email, message = message)
-        db.session.add(contact)
-        db.session.commit()
-        form.name.data = None
-        form.email.data = None
-        form.message.data = None
+    form = None
+    section = Sections.query.filter_by(name = "contact", is_active = True).first()
+    if section is not None: 
+        form = ContactForm()
+        name = None
+        email = None
+        message = None
+        if request.method == 'POST' and form.validate_on_submit():
+            name = form.name.data
+            email = form.email.data
+            message = form.message.data
+            contact = Contact(name = name, email = email, message = message)
+            db.session.add(contact)
+            db.session.commit()
+            form.name.data = None
+            form.email.data = None
+            form.message.data = None
     return render_template("contact.html", form = form)
 
 
